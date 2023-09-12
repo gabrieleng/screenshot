0fff326daea831a67c46add51bd41fd05244baee
Gabriel Eng <gabe4937@gmail.com>
2023-09-12 15:13:44 -0700

    Added tiktok

--- screenshot.js
+++ screenshot.js
@@ -12,6 +12,12 @@ function timeout(ms) {
 (async() => {
     while(1==1){
         let tracks = [
+            
+            {
+                "type": "tiktok",
+                "url": "https://www.tiktok.com/@gabe4937/video/6917089639392185606",
+                "playTime": 16
+            },
             {
                 "type": "youtube",
                 "url": "https://www.youtube.com/watch?v=ZD09qii5tNI",
@@ -37,7 +43,6 @@ function timeout(ms) {
                 "url": "https://soundcloud.com/djpge/dont-worry-baby-feat-candice-eng",
                 "playTime": 180
             }
-            
         ]
         let shuffled = tracks
             .map(value => ({ value, sort: Math.random() }))
@@ -48,7 +53,7 @@ function timeout(ms) {
             console.log(track);
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
-            await page.setViewport({width: 1000, height: 600})
+            await page.setViewport({width: 1000, height: 750})
             await page.goto(track.url, {waitUntil: 'networkidle2'});
 
             if(track.type=="youtube"){
@@ -63,8 +68,16 @@ function timeout(ms) {
                     document.querySelector('.sc-button-play.playButton.sc-button.m-stretch').click();
                 });
             }
+            if(track.type=="tiktok"){
+                /*
+                await page.waitForSelector('.css-q1bwae-DivPlayIconContainer.e1ya9dnw8');
+                await page.evaluate(() => {
+                    document.querySelector('.css-q1bwae-DivPlayIconContainer.e1ya9dnw8').click();
+                });
+                */
+            }
             
-            let playTime = (track.playTime - (Math.floor(Math.random() * 15)));
+            let playTime = (track.playTime - (Math.floor(Math.random() * .10*track.playTime)));
             await timeout(playTime*1000)
             await page.screenshot({path: `screenshots/${Date.now()}.jpg`});
             browser.close();
